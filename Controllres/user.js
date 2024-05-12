09be6251f9e34355b80ed10cae4eb685b9dac694
Awais Zubair <awaiszubair512@gmail.com>
2024-05-13 03:23:57 +0500

    changes

--- Controllres/user.js
+++ Controllres/user.js
@@ -1,9 +1,31 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
 
 const Register = async (req, res) => {
     const { name, email, phone, password } = req.body;
+
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: 'kickart11@gmail.com',
+            pass: 'iccj tbvg xzlg xckt'
+        }
+    });
+
+    const mailOptions = {
+        from: 'kickart11@gmail.com',
+        // to: `${user.email}`,
+        to: `findnickresearch@gmail.com`,
+        subject: 'Registration',
+        text: `Thank you for being the part of LinkedIn Account we will reach you shortly`
+    };
+
+
+
+
+    console.log(req.body);
     try {
         const data = { name, email, phone, password }
         let user = await User.findOne({ email: email })
@@ -21,7 +43,19 @@ const Register = async (req, res) => {
             }
         }
 
-        jwt.sign(payload, process.env.JWTSECRET || 'myscretkey', { expiresIn: 3600000 }, (err, token) => { if (err) throw err.message; return res.json({ token }) })
+        jwt.sign(payload, process.env.JWTSECRET || 'myscretkey', { expiresIn: 3600000 }, (err, token) => {
+            if (err) throw err.message;
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    //   res.send('Mail Error: ', error);
+                    console.log("Error: ", error);
+                } else {
+                    glob_email = email;
+                }
+            }
+            );
+            return res.json({ token }); console.log(token);
+        })
     } catch (error) {
         console.error(error.message);
         res.status(500).send('Server Error')
@@ -34,7 +68,7 @@ const Login = async (req, res) => {
     try {
         const user = await User.findOne({ email })
         if (!user) {
-            return res.status(400).json({ msg: 'User with this email doesnot exists' })
+            return res.status(200).json({ msg: 'User with this email doesnot exists' })
         }
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
